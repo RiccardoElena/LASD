0573e62de452dc6fea5976d43f08d1f1ebf0af8f
Riccardo Elena <witherheart63@gmail.com>
2024-03-14 10:07:15 +0100

    fix: fixed build script to take parameters

--- Lezioni/build.sh
+++ Lezioni/build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# check if compiler is given or is empty
+if [ -z "$1" ]; then
+  echo "Enter the compiler command (e.g. g++, clang++, etc...):"
+
+  read compiler
+else
+  compiler=$1
+fi
+
+# check if filename is given or is empty
+if [ -z "$2" ]; then
+  echo "Enter the name for the file to execute:"
+
+  read filename
+else
+  filename=$2
+fi
+
 # Get the directory of the script
 script_dir=$(dirname "$0")
-
-echo "Enter the compiler command (e.g. g++, clang++, etc...):"
-read compiler
-
-echo "Enter the name for the executable file:"
-read filename
 
 eval_output=$(eval "$script_dir/compile.sh $compiler $filename" 2>&1)
 # Check if there was an error
