92aaa999ba41d89958cfa71a04788fdc8f37568e
Riccardo Elena <witherheart63@gmail.com>
2024-03-19 01:02:47 +0100

    chore: moved to flag input for scripts

--- Lezioni/build.sh
+++ Lezioni/build.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
-# check if compiler is given or is empty
+# Default values
+compiler="g++"
+opt="0"
+path="."
+
+# Analizza le opzioni del comando
+while getopts c:o:p: option
+do
+  case "${option}"
+  in
+  c) compiler=${OPTARG};;
+  o) case ${OPTARG} in
+       0|1|2|3|s) opt=${OPTARG};;
+       *) opt="";;
+     esac;;
+  p) path=${OPTARG};;
+  esac
+done
+
+# check if filename is given or is empty
+if [ -z "$opt" ]; then
+  read -p "Grade of optimization not given or invalid. Please enter the grade of optimization (0, 1, 2, 3, s): " opt
+  while [[ ! $opt =~ ^[0123s]$ ]]; do
+    if [ -z "$opt" ]; then
+      opt="0"
+      break
+    fi
+    read -p "Invalid input. Enter the grade of optimization (0, 1, 2, 3, s): " opt
+  done
+fi
+
+# check if filename is given or is empty
 if [ -z "$1" ]; then
-  echo "Enter the compiler command (e.g. g++, clang++, etc...):"
-
-  read compiler
-else
-  compiler=$1
-fi
-
-# check if filename is given or is empty
-if [ -z "$2" ]; then
-  echo "Enter the grade of optimization (0,1,2,3,s):"
-
-  read opt
-else
-  opt=$2
-fi
-
-# check if path is given or is empty
-if [ -z "$3" ]; then
-  echo "Enter the path to the folder where the file cpp are located:"
-
-  read path
-else
-  path=$3
-fi
-
-# check if filename is given or is empty
-if [ -z "$4" ]; then
   echo "Enter the name for the file to execute:"
 
   read filename
 else
-  filename=$4
+  filename=$1
 fi
 
 # Get the directory of the script
