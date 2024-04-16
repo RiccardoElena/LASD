03e05cdd0bc86371ea2d237808cece0904768f29
Riccardo Elena <witherheart63@gmail.com>
2024-04-16 23:32:18 +0200

    ci: improved gh action

--- .github/workflows/scripts/compile.sh
+++ .github/workflows/scripts/compile.sh
@@ -1,4 +1,5 @@
  find ./.. -name makefile | while read makefile; do
             dir=$(dirname "$makefile")
             echo "compiling $dir/makefile"
+            make -C "$dir"
           done
--- .github/workflows/scripts/run.sh
+++ .github/workflows/scripts/run.sh
@@ -1,5 +1,6 @@
  find ./.. -name makefile | while read makefile; do
             dir=$(dirname "$makefile")
+            echo "compiling $dir/makefile"
             make -C "$dir"
             $dir/main
             result=$?
