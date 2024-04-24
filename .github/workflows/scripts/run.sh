c5b2587ce505da9fef384a7a6e9453d63a12d791
Riccardo Elena <witherheart63@gmail.com>
2024-04-24 20:39:28 +0200

    ci: uniformed scripts to same style

--- .github/workflows/scripts/run.sh
+++ .github/workflows/scripts/run.sh
@@ -1,7 +1,6 @@
-dir=$(ls -d ../exercise* | sort -V | tail -n 1)
 
-# Controlla se la cartella contiene un makefile
-if [ -f "$dir/makefile" ]; then
+
+find ./.. -name 'exercise*' -type d | sort -V | tail -n 1 | while read dir; do
     $dir/main a
     result=$?
     output="Test terminated with $result error"
@@ -13,6 +12,4 @@ if [ -f "$dir/makefile" ]; then
       echo "Test failed! Please check the code before pushing again."
       exit 1
     fi
-else
-    echo "No makefile found in $dir"
-fi
+done
