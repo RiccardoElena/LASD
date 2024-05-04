2c69be7fe51c12941ab37b88cdc4f25352084f76
Riccardo Elena <witherheart63@gmail.com>
2024-05-04 02:05:07 +0200

    test: testing g++ version

--- .github/workflows/scripts/compile.sh
+++ .github/workflows/scripts/compile.sh
@@ -1,4 +1,5 @@
 find ./.. -name 'exercise*' -type d | grep -P 'exercise\d+$' | sort -V | tail -n 1 | while read dir; do
+    g++ --version
     makefile="$dir/makefile"
     if [ -f "$makefile" ]; then
         echo "compiling $makefile"
